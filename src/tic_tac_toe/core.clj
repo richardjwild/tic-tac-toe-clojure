6a177dd499d47d04102cdde78cf5e9186d86f82f
Richard Wild <richardwild76@gmail.com>
2019-07-27 12:51:18 +0100

    Implemented

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,2 +1,60 @@
 (ns tic-tac-toe.core
   (:require [clojure.set :as set]))
+
+(def new-game {:state :game-on :to-play :x :board {}})
+
+(def winning-combos (list #{:top-left :top-middle :top-right}
+                          #{:centre-left :centre-middle :centre-right}
+                          #{:bottom-left :bottom-middle :bottom-right}
+                          #{:top-left :centre-left :bottom-left}
+                          #{:top-middle :centre-middle :bottom-middle}
+                          #{:top-right :centre-right :bottom-right}
+                          #{:top-left :centre-middle :bottom-right}
+                          #{:bottom-left :centre-middle :top-right}))
+
+(defn- advance [player]
+  (if (= player :x) :o :x))
+
+(defn- full? [board]
+  (= 9 (count board)))
+
+(defn- taken-by? [who [_ player]]
+  (= who player))
+
+(defn- squares-taken-by [who board]
+  (set (map first (filter (partial taken-by? who) board))))
+
+(defn- all-taken? [taken-squares combo]
+  (set/subset? combo taken-squares))
+
+(defn- won? [board who]
+  (some (partial all-taken? (squares-taken-by who board)) winning-combos))
+
+(defn- game-state [board]
+  (cond (full? board) :stalemate
+        (won? board :x) :x-has-won
+        (won? board :o) :o-has-won
+        :else :game-on))
+
+(defn- game-over? [state]
+  (contains? #{:stalemate :x-has-won :o-has-won} state))
+
+(defn- game
+  ([new-board new-state]
+   (hash-map :board new-board :state new-state))
+  ([new-board new-state to-play]
+   (hash-map :board new-board :state new-state :to-play to-play)))
+
+(defn- take-square [player square board]
+  (let [new-board (assoc board square player)
+        state (game-state new-board)]
+    (if (game-over? state)
+      (game new-board state)
+      (game new-board state (advance player)))))
+
+(defn play [game square]
+  (cond (game-over? (:state game)) game
+        :else (let [board (:board game)]
+                (if (contains? board square)
+                  (assoc game :state :square-already-played)
+                  (take-square (:to-play game) square board)))))
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -2,6 +2,93 @@
   (:require [midje.sweet :refer :all]
             [tic-tac-toe.core :refer :all]))
 
-(facts "about something"
-       (fact "zero equals zero"
-             (= 0 0) => true))
+(defn after-playing [& square]
+  (reduce play new-game square))
+
+(def stalemated-game (after-playing :top-left
+                                    :top-middle
+                                    :top-right
+                                    :centre-left
+                                    :centre-middle
+                                    :bottom-left
+                                    :centre-right
+                                    :bottom-middle
+                                    :bottom-right))
+
+(facts "about a game of tic tac toe"
+       (fact "x plays first"
+             (:state new-game) => :game-on
+             (:to-play new-game) => :x)
+       (fact "o plays after x"
+             (let [game (after-playing :top-left)]
+               (:state game) => :game-on
+               (:to-play game) => :o))
+       (fact "players alternate"
+             (let [game (after-playing :top-left :top-middle)]
+               (:state game) => :game-on
+               (:to-play game) => :x))
+       (fact "same square cannot be played twice"
+             (let [game (after-playing :top-left :top-middle :top-left)]
+               (:state game) => :square-already-played
+               (:to-play game) => :x)))
+
+(facts "about a stalemated game"
+       (fact "the board is full and neither player has three in a row"
+             (let [game stalemated-game]
+               (:state game) => :stalemate))
+       (fact "no-one is to play next"
+             (let [game stalemated-game]
+               (contains? game :to-play) => false))
+       (fact "further play is not permitted"
+             (let [game (play stalemated-game :top-left)]
+               (:state game) => :stalemate)))
+
+(facts "about a game won by x"
+       (fact "they have three in a row"
+             (let [game (after-playing :top-left :centre-left :top-middle :centre-middle :top-right)]
+               (:state game) => :x-has-won)
+             (let [game (after-playing :centre-left :top-left :centre-middle :top-middle :centre-right)]
+               (:state game) => :x-has-won)
+             (let [game (after-playing :bottom-left :top-left :bottom-middle :top-middle :bottom-right)]
+               (:state game) => :x-has-won)
+             (let [game (after-playing :top-left :top-right :centre-left :centre-right :bottom-left)]
+               (:state game) => :x-has-won)
+             (let [game (after-playing :top-middle :top-right :centre-middle :centre-right :bottom-middle)]
+               (:state game) => :x-has-won)
+             (let [game (after-playing :top-right :top-left :centre-right :centre-left :bottom-right)]
+               (:state game) => :x-has-won)
+             (let [game (after-playing :top-left :centre-left :centre-middle :top-middle :bottom-right)]
+               (:state game) => :x-has-won)
+             (let [game (after-playing :bottom-left :centre-left :centre-middle :bottom-middle :top-right)]
+               (:state game) => :x-has-won))
+       (fact "no-one is to play next"
+             (let [game (after-playing :top-left :centre-left :top-middle :centre-middle :top-right)]
+               (contains? game :to-play) => false))
+       (fact "further play is not permitted"
+             (let [game (after-playing :top-left :centre-left :top-middle :centre-middle :top-right :centre-right)]
+               (:state game) => :x-has-won)))
+
+(facts "about a game won by o"
+       (fact "they have three in a row"
+             (let [game (after-playing :bottom-left :top-left :centre-left :top-middle :centre-middle :top-right)]
+               (:state game) => :o-has-won)
+             (let [game (after-playing :bottom-left :centre-left :top-left :centre-middle :top-middle :centre-right)]
+               (:state game) => :o-has-won)
+             (let [game (after-playing :centre-left :bottom-left :top-left :bottom-middle :top-middle :bottom-right)]
+               (:state game) => :o-has-won)
+             (let [game (after-playing :top-middle :top-left :top-right :centre-left :centre-right :bottom-left)]
+               (:state game) => :o-has-won)
+             (let [game (after-playing :top-left :top-middle :top-right :centre-middle :centre-right :bottom-middle)]
+               (:state game) => :o-has-won)
+             (let [game (after-playing :top-middle :top-right :top-left :centre-right :centre-left :bottom-right)]
+               (:state game) => :o-has-won)
+             (let [game (after-playing :bottom-middle :top-left :centre-left :centre-middle :top-middle :bottom-right)]
+               (:state game) => :o-has-won)
+             (let [game (after-playing :centre-right :bottom-left :centre-left :centre-middle :bottom-middle :top-right)]
+               (:state game) => :o-has-won))
+       (fact "no-one is to play next"
+             (let [game (after-playing :bottom-left :centre-left :top-middle :centre-middle :top-right :centre-right)]
+               (contains? game :to-play) => false))
+       (fact "further play is not permitted"
+             (let [game (after-playing :bottom-left :top-left :centre-left :top-middle :centre-middle :top-right :centre-right)]
+               (:state game) => :o-has-won)))
